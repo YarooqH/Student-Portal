259490f3df0c5ab4685d8be8ec6640dced401d2d
Yarooq Anwar <yarooq1@gmail.com>
2021-07-18 18:16:26 +0500

    18 July

--- StudentPortal/src/App.java
+++ StudentPortal/src/App.java
@@ -5,3 +5,39 @@ public class App {
         System.out.println("Hello, World!");
     }
 }
+
+class Person {
+    String name;
+    String role;
+
+    public Person(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
+
+class Student {
+    final String seatNumber;
+    final int batch;
+
+    public Student(String seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+}
