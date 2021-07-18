2737084046df54cc9885601b5a8e9a12cbfe0f16
Hanan <hananjohn111@gmail.com>
2021-07-18 21:48:27 +0500

    Added more files

---
+++ grid.java
@@ -0,0 +1,8 @@
+public class grid{
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        double c = a*b;
+        System.out.println("Product of " + a + " and "+ b + " is " + c);
+    }
+}
