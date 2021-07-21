a157be4d04993375a1ce9298c256271ebf4feab5
gowther33 <84916771+gowther33@users.noreply.github.com>
2021-07-21 21:34:39 +0500

    Delete grid.java

--- grid.java
+++
@@ -1,8 +0,0 @@
-public class grid{
-    public static void main(String[] args) {
-        int a = 2;
-        int b = 3;
-        double c = a*b;
-        System.out.println("Product of " + a + " and "+ b + " is " + c);
-    }
-}
