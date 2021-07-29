175dd169278b63372a06c8fe7593fc064604eaaa
Yarooq Anwar <yarooq1@gmail.com>
2021-07-29 09:40:52 +0500

    GUI fxmls done

---
+++ StudentPortal/src/GUI.java
@@ -0,0 +1,26 @@
+package StudentPortal.src;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class GUI extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("./fxml/LoginForm-Admin.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
